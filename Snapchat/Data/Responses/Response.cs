4f7b600257dc2ca99cc9f56d623bc703b1068786
msaville8@gmail.com <msaville8@gmail.com>
2014-11-19 20:40:56 -0500

    Added missing file: Response.cs, and created blank Windows Phone app to be used during development

---
+++ Snapchat/Data/Responses/Response.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Matt Saville and Alex Forbes-Reed. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Newtonsoft.Json;
+
+namespace Snapchat.Data.Responses
+{
+	/// <summary>
+	/// Represents a basic response from the Snapchat API.
+	/// </summary>
+	public class Response
+	{
+		/// <summary>
+		/// Gets whether the request has been logged.
+		/// </summary>
+		[JsonProperty("logged")]
+		public bool IsLogged { get; private set; }
+
+		/// <summary>
+		/// Gets the message passed along with the response.
+		/// </summary>
+		[JsonProperty("message")]
+		public string Message { get; private set; }
+	}
+}
